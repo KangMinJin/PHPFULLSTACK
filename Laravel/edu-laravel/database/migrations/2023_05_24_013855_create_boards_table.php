002f27489ba352d9a8bf6eb59b419d2274cf8e67
KangMinJin <kmj_7809@naver.com>
2023-05-24 16:42:33 +0900

    2023-05-24

---
+++ Laravel/edu-laravel/database/migrations/2023_05_24_013855_create_boards_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    // migration 파일 생성 : php artisan make:migration create_boards_table
+    // migration 실행(모든 migration 파일의 up()메소드를 실행) : php artisan migrate
+    // migration 리셋(모든 migration 파일의 down()메소드를 실행) : php artisan migrate:reset
+    // migration 롤백(가장 마지막에 실행한 migration의 내용을 롤백) : php artisan migrate:rollback
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            // 글 번호, 제목, 내용, 작성일, 수정일, 삭제일자, 삭제여부
+            $table->id('bno'); // big_int, pk, auto_increment
+            $table->char('category', 1)->index(); // char(1), not null, index추가
+            $table->string('btitle', 100); // varchar(100), not null
+            $table->string('bcontent', 4000); // varchar(4000), not null
+            $table->timestamps(); // created_at, updated_at을 자동 생성, null 허용
+            $table->timestamp('deleted_at')->nullable();
+            $table->char('deleted_flg', 1)->default('0'); // char(1), default '0', not null
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+};
--- Laravel/edu-laravel/database/seeders/DatabaseSeeder.php
+++ Laravel/edu-laravel/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Database\Seeders\CategorySeeder; // 호출할 seeder use
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,6 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // 초기 데이터 삽입용 seeder 호출
+        $this->call(CategorySeeder::class);
+
+        // 더미 데이터 삽입용 factory 호출
+        $cnt = 0;
+        while ($cnt < 5) {
+            \App\Models\Board::factory(10000)->create();
+            $cnt++;
+        }
+        // * 반복문 돌리면 5만건 들어갈 수 있음.
     }
 }
